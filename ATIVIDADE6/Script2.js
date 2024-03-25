7617357b8f38bfa786224b5924491addf897387d
Brennon Lima <130801020+BrennonMLima@users.noreply.github.com>
2024-03-25 12:25:02 -0300

    Add files via upload

---
+++ ATIVIDADE6/Script2.js
@@ -0,0 +1,15 @@
+var n1, n2;
+
+n1 = prompt("Digite o número 1: ")
+n2 = prompt("Digite o número 2: ")
+
+var num1 = parseFloat(n1);
+var num2 = parseFloat(n2);
+
+alert("Soma dos dois números: " + (num1 + num2) + 
+    "\rSubtração do primeiro pelo segundo: " + (num1 - num2) +
+    "\rO produto dos dois: " + (num1 * num2) +
+    "\rA divisão do primeiro pelo segundo: "+(num1 / num2) + 
+    "\rO resto da divisão do primeiro pelo segundo: " + (num1 % num2));
+
+
